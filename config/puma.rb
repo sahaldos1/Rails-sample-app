d05378d27411e021d5e46ffb90d471058bcc58c4
Sahal Asghar <sahaldos1@gmail.com>
2020-07-10 18:22:46 -0400

    Attempted deploymet crash fix

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,12 @@
 # Puma configuration file. taken from Heroku Docs after setting config.forcessl to true in production.rb
 
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 } 
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count } threads min_threads_count, max_threads_count
-
-port ENV.fetch("PORT") { 3000 }
-
-environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "development" } pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# Puma configuration file.
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "development" }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 plugin :tmp_restart
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  
+
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
