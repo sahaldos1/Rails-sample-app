b428ea3d660c9c0cfa711899d15e625f085e24b3
Sahal Asghar <sahaldos1@gmail.com>
2020-07-11 19:26:34 -0400

    implement integration test for login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,14 +2,23 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   
-  test "login with invalid information" do
+  
+  def setup
+    @user = users(:Jim)
+  end
+ 
+
+  test "login with valid information" do
     get login_path
-    assert_template 'session/new'
-    post login_path, params: { session: { email: "", password: "" } } 
-    assert_template 'session/new'
-    assert_not flash.empty? 
-    get root_path
-    assert flash.empty?
+    post login_path, params: { session: { email: @user.email,
+    password: 'password' }}
+
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0 
+    assert_select "a[href=?]", logout_path 
+    assert_select "a[href=?]", user_path(@user)
   end
 
 
