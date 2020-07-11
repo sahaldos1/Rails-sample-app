9d0ae25f0073c63cec6e52030bd4772571162512
Sahal Asghar <sahaldos1@gmail.com>
2020-07-11 18:08:52 -0400

    add warning flash upon invalid user login

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  get '/login', to: 'sessions#new'
+  get '/login', to: 'session#new'
   post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
   resources :users
