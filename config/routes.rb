5d502aa5de26fb49b6ba719c2c2a99c8bc19e179
Sahal Asghar <sahaldos1@gmail.com>
2020-07-11 17:50:42 -0400

    Add session controller and relevant routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'session/new'
   get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -9,5 +10,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
   resources :users
 end
