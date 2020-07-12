ea7d7b7c2bc95e2bd4bfa67609842524dd71c361
Sahal Asghar <sahaldos1@gmail.com>
2020-07-11 21:10:44 -0400

    Implement log out functionality

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -17,6 +17,8 @@ class SessionController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 
 end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -17,5 +17,11 @@ module SessionHelper
     !current_user.nil?
   end
 
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil 
+  end
+
 
 end
