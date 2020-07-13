860ba0d09ff07ceab942b334ac9d4e55df9bc07f
Sahal Asghar <sahaldos1@gmail.com>
2020-07-13 16:12:03 -0400

    Add edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,17 @@ class UsersController < ApplicationController
   def edit 
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Handle a successful update.
+    else
+      render 'edit' 
+    end
+  end
   
+
 
   private 
 
