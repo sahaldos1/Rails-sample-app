7e569ae57f14f005904aade4444db616cafe5f04
Sahal Asghar <sahaldos1@gmail.com>
2020-07-14 15:31:42 -0400

    Add activation tokens

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,9 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now
              )
 
 # Generate a bunch of additional users.
@@ -23,5 +25,7 @@ User.create!(name:  "Example User",
               email: email,
               password:              password,
               password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now
               )
 end
