d97cd1f023a091c0ce90e7d3a8cc7d89d128e6a4
Sahal Asghar <sahaldos1@gmail.com>
2020-07-15 14:44:35 -0400

    Allow for user microposts to be retrieved in descending order and make microposts dependant on users

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -27,6 +27,9 @@ class MicropostTest < ActiveSupport::TestCase
     assert_not @micropost.valid?
   end
 
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first 
+  end
 
 
 end
