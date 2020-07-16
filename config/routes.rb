74b3144c3ce91fa0ab66adac3fcafff8c92358a3
Sahal Asghar <sahaldos1@gmail.com>
2020-07-16 16:43:19 -0400

    Implement views for follow and unfollow components

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end 
+  end
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
